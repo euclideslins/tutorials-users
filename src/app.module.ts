5441c5db5928c37bd92c4d515a60c56df7931e00
Euclides Lins <euclidesvasconcelos01@gmail.com>
2024-08-27 01:32:10 -0300

    chore: add typeorm and mysql structre

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TutorialModule } from './tutorial/tutorial.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TutorialModule, UsersModule],
+  imports: [TutorialModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'tutorialusers',
+      entities: [],
+      synchronize: true,
+    }),
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
